4310800b5281b654339bb174f5f80bf52b7db9c6
victorcvs <victorcvs@gmail.com>
2020-06-10 18:46:28 -0300

    Projeto criado

---
+++ src/test/java/com/victorcvs/workshopmongo/WorkshopSpingMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorcvs.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpingMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
