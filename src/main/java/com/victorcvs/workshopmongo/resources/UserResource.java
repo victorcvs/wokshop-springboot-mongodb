489a8c4cab7722d7bac8697549fd988319fdebf6
victorcvs <victorcvs@gmail.com>
2020-06-12 16:23:57 -0300

    Atualização de usuário com PUT

--- src/main/java/com/victorcvs/workshopmongo/resources/UserResource.java
+++ src/main/java/com/victorcvs/workshopmongo/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -58,6 +59,14 @@ public class UserResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@PathVariable String id , @RequestBody UserDTO objDto) {
+		User user = service.fromDto(objDto);
+		user.setId(id);
+		user = service.update(user);
+		return ResponseEntity.noContent().build();
 	}
 
 }
