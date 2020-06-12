6adfa7408990d4294b977a00c3faee2e97e2ae54
victorcvs <victorcvs@gmail.com>
2020-06-12 19:08:54 -0300

    Consulta simples com @Query

--- src/main/java/com/victorcvs/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/victorcvs/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.victorcvs.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.victorcvs.workshopmongo.domain.Post;
@@ -11,5 +12,10 @@ import com.victorcvs.workshopmongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String>{
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title' : { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
+	
 	
 }
